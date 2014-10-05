4cddeb82096c12f08e82ad9b89f1fbbe7f34c011
Mark Dietzer <mriq91@gmail.com>
2014-10-05 16:15:58 +0200

    Split up protocols package

--- protocols/config.go
+++ protocols/config.go
@@ -6,63 +6,6 @@ import (
 	"encoding/json"
 )
 
-type ProtocolMatcher interface {
-	Matches(data []byte) bool
-	
-	GetTarget() string
-	GetProtocol() string
-	
-	setProtocol(protocol string)
-	setTarget(target string)
-}
-
-type protocolMatcherBase struct {
-	target string
-	protocol string
-}
-
-func (p *protocolMatcherBase) GetTarget() string {
-	return p.target
-}
-
-func (p *protocolMatcherBase) GetProtocol() string {
-	return p.protocol
-}
-
-func (p *protocolMatcherBase) setTarget(target string) {
-	p.target = target
-}
-
-func (p *protocolMatcherBase) setProtocol(protocol string) {
-	p.protocol = protocol
-}
-
-type protocolMatcherRegexp struct {
-	protocolMatcherBase
-	regexp *regexp.Regexp
-}
-
-func (p *protocolMatcherRegexp) Matches(data []byte) bool {
-	return p.regexp.Match(data)
-}
-
-type protocolMatcherLiteral struct {
-	protocolMatcherBase
-	matchPattern []int
-}
-
-func (p *protocolMatcherLiteral) Matches(data []byte) bool {
-	if len(data) < len(p.matchPattern) {
-		return false
-	}
-	for i, b := range p.matchPattern {
-		if b >= 0 && data[i] != byte(b) {
-			return false
-		}
-	}
-	return true
-}
-
 type ProxyProtocolConfig struct {
 	Matchers []ProtocolMatcher
 }
